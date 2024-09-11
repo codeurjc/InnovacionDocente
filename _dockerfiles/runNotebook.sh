feb1757f51a809cc03f66cd09bf37e127df14974
Michel Maes <michel.maes95@gmail.com>
2024-09-11 18:13:24 +0200

    Update Preliminary Analysis + translate results

--- _dockerfiles/runNotebook.sh
+++ _dockerfiles/runNotebook.sh
@@ -1,1 +1,5 @@
+if [ -z "$(docker images -q innovacion-docente 2> /dev/null)" ]; then
+  docker build -f _dockerfiles/Dockerfile -t innovacion-docente .
+fi
+
 docker run --rm -d -p 8888:8888 -v $PWD:/home/jovyan/work/ innovacion-docente
